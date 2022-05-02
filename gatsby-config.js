4afbce5732caa89fb68636b3f720daf207f0b07e
dloynaz <dloynaz@hotmail.com>
2022-05-02 16:12:23 -0600

    frontend fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,19 +44,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     `gatsby-theme-material-ui`,
     {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          'gatsby-remark-relative-images',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false
-            }
-          }
-        ]
-      }
+      resolve: "gatsby-transformer-remark" 
     },
     {
       resolve: `gatsby-source-contentful`,
