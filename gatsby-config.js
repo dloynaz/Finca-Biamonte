259531335f2473ba265fce88fd566cca8d9b50c2
dloynaz <dloynaz@hotmail.com>
2022-05-02 15:50:28 -0600

    node fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,8 +62,24 @@ module.exports = {
         spaceId: "ma4rt5elvesx",
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
   ],
+  
 
 }
 
